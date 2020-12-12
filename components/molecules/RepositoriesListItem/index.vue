518f74f10d4fd08267f69c8847ee2ebdcbf16705
Harrison Vinicius <harrison_vms@hotmail.com>
2020-12-12 19:17:24 -0300

    implementacao do layout do item da lista de repositorios e inicio da estruturacao da store

--- components/molecules/RepositoriesListItem/index.vue
+++ components/molecules/RepositoriesListItem/index.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="repositories-list-item">
-
-    </div>
+        <div class="avatar-card">
+            <img class="avatar-card__image" src="@/assets/hub.png" alt="avatar">
+        </div>
+        <div class="repositories-list-item__title">
+            <span class="repositories-list-item__title__text-decoration"> Repository title </span>
+        </div>
+        <div class="repositories-list-item__action">
+            <span> salvar </span>
+        </div>  
+    </div>  
 </template>
 
 <script>
@@ -17,5 +25,37 @@ export default {
     border-radius: 5px;
     margin-bottom: 15px;
     background-color: #ededed;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    &__title{
+        height: 100%;
+        width: 85%;
+        &__text-decoration{
+            color: black;
+            font-size: 20px;
+            margin-left: 15px;
+        }
+    }
+    &__action{
+        width: 10%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+}
+
+.repositories-list-item:hover{
+    opacity: 0.7;
+}
+
+.avatar-card{
+    height: 100%;
+    width: 190px;
+    &__image{
+        height: 100%;
+        width: 100%;
+    }
 }
 </style>
